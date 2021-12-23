1b2a153f1439aceb9d4c19d9f7fe6dd155182d6a
Samuel Jiang <samueljiang@outlook.com>
2021-12-23 14:26:38 -0500

    test

--- src/python/utils/ts_backup_utils_cmd.py
+++ src/python/utils/ts_backup_utils_cmd.py
@@ -139,7 +139,7 @@ def dataBackup():
     print('(2) Simplified (A progress bar with single line outputs')
     select = input('\nEnter Selection: ')
     if select == 1:
-        data_backup.backupData(customer_name, ticket_id, mountpoint, driveSelected[4], usersSelected)
+        data_backup.backupData(customer_name, ticket_id, mountpoint, driveSelected[4], usersSelected, verbose=True)
     else:
         data_backup.backupData(customer_name, ticket_id, mountpoint, driveSelected[4], usersSelected, verbose=False)
     input()
