e658cba51eddb8fee1595af79aa178d7e7d2aab2
Samuel Jiang <samueljiang@outlook.com>
2021-12-23 14:01:36 -0500

    Added non-verbose

--- src/python/utils/rsync.py
+++ src/python/utils/rsync.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, run
 import os
 import time
 import re
@@ -87,63 +87,74 @@ def rsync_run_simple(excludes:list, src, dest):
         if not os.path.exists(dest):
             print('Created Dest')
             os.mkdir(dest)
-        
+
         rsyncCMD = ['rsync', '-rhv', '--progress', '--update', '--safe-links']
         for exclude in excludes:
             rsyncCMD.append('--exclude=' + exclude)
         rsyncCMD.append(src)
         rsyncCMD.append(dest)
 
-        p = Popen(rsyncCMD, stdout=PIPE, stderr=STDOUT, text=True)
+        # Get some stats before running
+        print('Capturing Stats')
+        rsyncStatCMD = ['rsync', '-rh', '--update', '--safe-links', '--dry-run', '--stats']
+        for exclude in excludes:
+            rsyncStatCMD.append('--exclude=' + exclude)
+        rsyncStatCMD.append(src)
+        rsyncStatCMD.append(dest)
+        result = run(rsyncStatCMD, capture_output=True)
+        output = result.stdout.decode('utf-8').splitlines()
+        print(result)
 
-        # Log progress
-        timeout_base = 0
-        timeout_limit = 100000 # 100 seconds
-        while True:
-            if getMS() - timeout_base >= timeout_limit and timeout_base != 0:
-                print('Some process taking too long')
+        # p = Popen(rsyncCMD, stdout=PIPE, stderr=STDOUT, text=True)
 
-            line =  p.stdout.readline()
+        # # Log progress
+        # timeout_base = 0
+        # timeout_limit = 100000 # 100 seconds
+        # while True:
+        #     if getMS() - timeout_base >= timeout_limit and timeout_base != 0:
+        #         print('Some process taking too long')
+
+        #     line =  p.stdout.readline()
             
-            # Stop when stream ended
-            if not line:
-                break
+        #     # Stop when stream ended
+        #     if not line:
+        #         break
             
-            line = line.strip()
-            splitLine = line.split()
+        #     line = line.strip()
+        #     splitLine = line.split()
             
-            if line != '':
-                # Start Line
-                if line == 'sending incremental file list':
-                    print('Reading directories')
-                    timeout_base = getMS()
-                # Get the skip outputs
-                elif 'skipping' in line:
-                    print(line)
-                # 2nd to the last output
-                elif 'sent' in line:
-                    sentSize = splitLine[1]
-                    avgSpeed = splitLine[6] + ' ' + splitLine[7]
-                    print('Sent: ' + sentSize + '; Speed: ' + avgSpeed)
-                # Last output
-                elif 'total size' in line:
-                    totalSize = splitLine[3]
-                    print('Total Size: ' + totalSize)
-                # Getting a new file
-                elif splitSrc[-1] in line and 'B/s' not in line:
-                    # Reset timer
-                    timeout_base = getMS()
-                    print('Copying: ' + line, end='\r')
-                # Probably the progress?
-                elif 'B/s' in line:
-                    copiedSize = splitLine[0]
-                    progress = splitLine[1]
-                    speed = splitLine[2]
-                    timeLeft = splitLine[3]
-                    print('Copying: ' + copiedSize + ' Percentage Completed: ' + progress + ' Rate: ' + speed + ' Estimated Time Left: ' + timeLeft, end='\r')
-                    if len(splitLine) == 6:
-                        numFileTransferred = int(re.search(r'\d+', splitLine[4]).group())
-                        transferStat = list(map(int, re.findall(r'\d+', splitLine[5])))
-                        print('\nCopied File #' + str(numFileTransferred) + ' There are ' + str(transferStat[0]) + '/' + str(transferStat[1]) + ' left')
+        #     if line != '':
+        #         # Start Line
+        #         if line == 'sending incremental file list':
+        #             print('Reading directories')
+        #             timeout_base = getMS()
+        #         # Get the skip outputs
+        #         elif 'skipping' in line:
+        #             print(line)
+        #         # 2nd to the last output
+        #         elif 'sent' in line:
+        #             sentSize = splitLine[1]
+        #             avgSpeed = splitLine[6] + ' ' + splitLine[7]
+        #             print('Sent: ' + sentSize + '; Speed: ' + avgSpeed)
+        #         # Last output
+        #         elif 'total size' in line:
+        #             totalSize = splitLine[3]
+        #             print('Total Size: ' + totalSize)
+        #         # Getting a new file
+        #         elif splitSrc[-1] in line and 'B/s' not in line:
+        #             # Reset timer
+        #             timeout_base = getMS()
+        #             print('Copying: ' + line, end='\r')
+        #         # Probably the progress?
+        #         elif 'B/s' in line:
+        #             copiedSize = splitLine[0]
+        #             progress = splitLine[1]
+        #             speed = splitLine[2]
+        #             timeLeft = splitLine[3]
+        #             print('Copying: ' + copiedSize + ' Percentage Completed: ' + progress + ' Rate: ' + speed + ' Estimated Time Left: ' + timeLeft, end='\r')
+        #             if len(splitLine) == 6:
+        #                 numFileTransferred = int(re.search(r'\d+', splitLine[4]).group())
+        #                 transferStat = list(map(int, re.findall(r'\d+', splitLine[5])))
+        #                 print('\nCopied File #' + str(numFileTransferred) + ' There are ' + str(transferStat[0]) + '/' + str(transferStat[1]) + ' left')
     else:
         raise FileNotFoundError('Source Directory Not Found!')
